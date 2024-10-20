8f76b3dd908072e2e1dcf669f063299f0c4f2116
PashaSamudraPratamaaa <pasha.pratama_ti24@nusaputra.ac.id>
2024-10-20 14:18:22 +0700

    Add files via upload

---
+++ Ab.java
@@ -0,0 +1,9 @@
+package TugasPerulangan;
+
+public class Ab {
+    public static void main(String[] args) {
+        for(int i =25; i>= -45;i -= 5){
+        System.out.println(i +"");
+        }
+    }
+}
---
+++ Ad.java
@@ -0,0 +1,13 @@
+package TugasPerulangan;
+
+public class Ad {
+    public static void main(String[] args)  {
+        int x =1 , a=1;
+        while (x<=121) {
+            System.out.println(x + "");
+            x = x + a;
+            a = a + 1;
+            
+        }
+    }
+}
